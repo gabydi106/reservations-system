1050ab221cf16783414fd9add2ce84da4868eba9
Gaby Di <gabydi106@gmail.com>
2017-12-19 11:59:43 +0800

    Add count of available beds

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,3 +1,11 @@
 class Bed < ApplicationRecord
   belongs_to :reservation, optional: true
+
+  def is_available
+    where("available is TRUE")
+  end
+
+  def self.count_available
+    where("available is TRUE").count
+  end
 end
